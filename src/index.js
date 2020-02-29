0fc9e18cbf05ede6b2e8d9616845e718cdeb5181
rehan-sattar <rehansattar117@gmail.com>
2020-03-01 00:19:17 +0500

    Add docs for Functions, Objects & Classes

--- src/index.js
+++ src/index.js
@@ -18,3 +18,66 @@ const todo = {
   id: 'SomeId',
   description: 'Some Description'
 };
+
+/**
+ *
+ * @description Calculates the total amount with tax
+ * @param {number} total - total amount
+ * @param {number} tax  - tax percentage
+ * @returns {string} - total with tax.
+ */
+const calculateTax = (total, tax) => {
+  return `${total + total * tax}`;
+};
+
+/**
+ * @typedef {Object} Student -
+ * @property {number} id - id of the student
+ * @property {string} name - name of student
+ * @property {number} age - age of student
+ * @property {boolean} isActive - activation status of student
+ */
+
+/**
+ * @type {Student}
+ * @description student object
+ */
+const student = {
+  id: 64,
+  name: 'Rehan Sattar',
+  age: 21,
+  isActive: false
+};
+
+/**
+ * @description Class to create a person
+ */
+class Person {
+  /**
+   * @param {Object} personInfo Information about person.
+   */
+  constructor(personInfo) {
+    /**
+     * @property {string} name - person's name
+     */
+    this.name = personInfo.name;
+    /**
+     * @property {number} age - person's age
+     */
+    this.age = personInfo.age;
+  }
+
+  /**
+   * @property {Function} greet A greeting with the name & age
+   * @returns {void}
+   */
+  greet() {
+    console.log(`My name is: ${this.name}, I'm ${this.age} years old`);
+  }
+}
+
+/**
+ * @description Person object
+ * See {@link Person}
+ */
+const person01 = new Person({ name: 'Rehan Sattar', age: 21 });
