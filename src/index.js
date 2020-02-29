7efa57b5cfdd57fa1fe8dc3dbfa385dfdabab6fc
rehan-sattar <rehansattar117@gmail.com>
2020-02-29 23:20:51 +0500

    Add comments for variables

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * @type {String}
+ * @description Student Name
+ */
+const studentName = 'Rehan Sattar';
+
+/**
+ * @type {Array<Number>}
+ * @description Grades of student
+ */
+const grades = [12, 23.4, 43.5, 10];
+
+/**
+ * @type {{ id: String, description: String}}
+ * @description Todo Example Object
+ */
+const todo = {
+  id: 'SomeId',
+  description: 'Some Description'
+};
